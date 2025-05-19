2d3501ad58efa17fc50f39d486007bd11493f544
Lingtum <127567333+Lingtum@users.noreply.github.com>
2025-05-19 11:59:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,11 @@ check_url = '{}/user/checkin'.format(url)
 
 
 header = {
-        'origin': url,
-        'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+    'origin': url,
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    'referer': url,
+    'accept-language': 'en-US,en;q=0.9',
+    'accept': 'application/json, text/javascript, */*; q=0.01',
 }
 data = {
         'email': email,
